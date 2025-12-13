2fdfafeaf30a06edaf98cfd108127caaea4b1e8b
Shivani Pandey <shivani@ipayments.org.in>
2025-12-13 18:21:35 +0530

    oneway added

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -151,6 +151,7 @@ class FlightController extends Controller
         $response = $service->bookingFlight($request->all());
 
 
+        dd($response);
         // return response()->json($response);
         if ($response['status'] != 'success') {
             return response()->json([
@@ -261,6 +262,109 @@ class FlightController extends Controller
         $service = new FlightService();
         $response = $service->FlightTicketView($request->all());
 
-        return response()->json($response);
+        // dd($response);
+        if ($response['status'] != 'success') {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Flight booking failed!'
+            ], 400);
+        }
+
+        $data = $response['data']['Response']['Response'] ?? null;
+
+
+        if (!$data) {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Invalid, Something went worng'
+            ], 400);
+        }
+
+
+        // Extracting required fields
+        $pnr              = $data['PNR'] ?? null;
+        $bookingId        = $data['BookingId'] ?? null;
+        $islcc        = $data['FlightItinerary']['IsLCC'] ? 'true' : 'false';
+        $isrefund        = $data['FlightItinerary']['NonRefundable'] ? 'true' : 'false';
+        $fare             = $data['FlightItinerary']['Fare'] ?? [];
+
+
+        $seg        = $data['FlightItinerary']['Segments'] ?? null;
+        $segments         = $seg[0] ?? null;
+
+        // NotSet = 0, Successful = 1, Failed = 2, OtherFare = 3, OtherClass = 4, BookedOther = 5, NotConfirmed = 6]
+        $status = "";
+        if ($data['Status'] = 0) {
+            $status = "Not Set";
+        } else  if ($data['Status'] = 1) {
+            $status = "Successful";
+        } else  if ($data['Status'] = 2) {
+            $status = "Failed";
+        } else  if ($data['Status'] = 3) {
+            $status = "OtherFare";
+        } else  if ($data['Status'] = 4) {
+            $status = "OtherClass";
+        } else  if ($data['Status'] = 5) {
+            $status = "BookedOther";
+        } else if ($data['Status'] = 6) {
+            $status = "NotConfirmed";
+        }
+
+        // segment length
+
+        $lastSegment = $seg[count($seg) - 1];
+
+
+        // Flight Details
+        $origin           = $segments['Origin']['Airport']['AirportCode'] ?? null;
+        $originName           = $segments['Origin']['Airport']['AirportName'] ?? null;
+        $destination = $lastSegment['Destination']['Airport']['AirportCode'] ?? null;
+        $destinationName = $lastSegment['Destination']['Airport']['AirportName'] ?? null;
+
+        $airlineCode      = $segments['Airline']['AirlineCode'] ?? null;
+        $airlineName      = $segments['Airline']['AirlineName'] ?? null;
+
+        $flightNumber     = $segments['Airline']['FlightNumber'] ?? null;
+        $journeyDate      = $segments['Origin']['DepTime'] ?? null;
+
+        // Fare details
+        $baseFare         = $fare['BaseFare'] ?? 0;
+        $tax              = $fare['Tax'] ?? 0;
+        $totalAmount      = ($fare['PublishedFare'] ?? 0);
+
+        // Store in DB
+        $booking = [
+            'user_id'         => \Auth::user()->id,
+            'pnr'             => $pnr,
+            'booking_id_api'  => $bookingId,
+            'origin'          => $origin . "-" .  $originName,
+            'destination'     => $destination . "-" .  $destinationName,
+            'airline_code'    => $airlineCode . "-" .  $airlineName,
+            'flight_number'   => $flightNumber,
+            'journey_date'    => $journeyDate,
+
+            'base_fare'       => $baseFare,
+            'tax'             => $tax,
+            'total_amount'    => $totalAmount,
+            'is_refundable'    => $isrefund,
+            'is_lcc'    => $islcc,
+
+            'payment_status'  => 'success',
+            'booking_status'  => $status,
+
+            'raw_response'    => json_encode($response['data']),
+
+            'created_at'      => now(),
+            'updated_at'      => now(),
+        ];
+
+        DB::table('bookings')->insert($booking);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Flight Booking Successfully',
+            'data' => $response['data']
+        ]);
+        // return response()->json($response);
     }
 }
