af13f6ecf11cabc654579dfbd013ff3ec066466b
Shivani Pandey <shivani@ipayments.org.in>
2025-12-19 18:21:33 +0530

    roundtrip ssr api and ui added

--- resources/views/flight/detail.blade.php
+++ resources/views/flight/detail.blade.php
@@ -305,7 +305,7 @@
             <div class="modal-content">
 
                 <div class="modal-header">
-                    <h5 class="modal-title" id="ruleFarelabel">Fare Rules</h5>
+                    <h5 class="modal-title" id="ruleFarelabel">Fare Rules Departure</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
@@ -322,7 +322,7 @@
             <div class="modal-content">
 
                 <div class="modal-header">
-                    <h5 class="modal-title" id="ruleFareRetlabel">Fare Rules</h5>
+                    <h5 class="modal-title" id="ruleFareRetlabel">Fare Rules Return</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
