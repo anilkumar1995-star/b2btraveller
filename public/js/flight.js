eeff91f31f6460152bdd43ba523e688c88acf59d
Shivani Pandey <shivani@ipayments.org.in>
2025-12-23 11:11:15 +0530

    view ticket api integrated

--- public/js/flight.js
+++ public/js/flight.js
@@ -2119,8 +2119,7 @@ function checkFinalBookingStatus(trip, journeyType) {
 
             swal({
                 title: "Booking Successful!",
-                html: `
-                <p>Your ticket has been booked successfully 🎉</p>
+                html: `<p>Your ticket has been booked successfully 🎉</p>
                 <p><strong>PNR:</strong> ${dep?.PNR || "Not Available"}</p>
                 <p><strong>Booking Id:</strong> ${dep?.BookingId || "Not Available"}</p>
             `,
