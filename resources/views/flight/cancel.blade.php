8dde3ba40179af8a92aa5dd9d58d32b427ab37a6
Shivani Pandey <shivani@ipayments.org.in>
2025-12-26 16:31:57 +0530

    view detaisl added

--- resources/views/flight/cancel.blade.php
+++ resources/views/flight/cancel.blade.php
@@ -31,6 +31,7 @@
 
                     <div class="col-md-6 h-100">
                         @foreach ($finalDet['Passenger'] as $p)
+                        @dd($p);
                             <div class="form-check mb-2">
                                 <input class="form-check-input" type="checkbox" name="ticket_ids[]"
                                     value="{{ $p['Ticket']['TicketId'] }}" checked required>
