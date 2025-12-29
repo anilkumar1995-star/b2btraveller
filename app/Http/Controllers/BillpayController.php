e3f01e4ad1f61456f4cc51decb2b5070efdcff1c
Shivani Pandey <shivani@ipayments.org.in>
2025-12-29 16:50:36 +0530

    ticket api done

--- app/Http/Controllers/BillpayController.php
+++ app/Http/Controllers/BillpayController.php
@@ -145,7 +145,8 @@ class BillpayController extends Controller
                 // case 'iydaBillpay':
                 $post['billerId'] = $provider->billerId;
                 $result = $this->callIydaBillpay->fetchBillPay($post, $provider, $user);
-                
+
+                dd($result);
                 if ($result['status']) {
                     return response()->json(['statuscode' => "TXN", "data" => $result['data']]);
                 } else {
