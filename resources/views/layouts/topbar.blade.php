c08fc4adb1dae10061294317730ff2c5b5b1cc7b
Shivani Pandey <shivani@ipayments.org.in>
2025-12-29 14:15:56 +0530

    Remove storage session files from git tracking

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -98,13 +98,13 @@
                                 <a href="{{ route('statement', ['type' => 'account']) }}" class="stretched-link">Main Wallet</a>
                                 <small class="text-muted mb-0">&#8377; {{Auth::user()->mainwallet}} /-</small>
                             </div>
-                            <div class="dropdown-shortcuts-item col">
+                            {{-- <div class="dropdown-shortcuts-item col">
                                 <span class="dropdown-shortcuts-icon rounded-circle mb-2">
                                     <i class="ti ti-file-invoice fs-4"></i>
                                 </span>
                                 <a href="{{ route('statement', ['type' => 'awallet']) }}" class="stretched-link">AEPS Wallet</a>
                                 <small class="text-muted mb-0"> &#8377; {{Auth::user()->aepsbalance}} /-</small>
-                            </div>
+                            </div> --}}
                              <div class="dropdown-shortcuts-item col">
                                 <span class="dropdown-shortcuts-icon rounded-circle mb-2">
                                     <i class="ti ti-file-invoice fs-4"></i>
