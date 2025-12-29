332b26299c7cd94eec743d9481ead0c2668e8386
Shivani Pandey <shivani@ipayments.org.in>
2025-12-29 17:21:46 +0530

    ticket api done

--- resources/views/statement/billpay.blade.php
+++ resources/views/statement/billpay.blade.php
@@ -58,7 +58,7 @@ $status['data'] = [
 </div>
 
 <div class="modal fade" id="receipt" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">      
                 <h4 class="modal-title">Receipt</h4>
