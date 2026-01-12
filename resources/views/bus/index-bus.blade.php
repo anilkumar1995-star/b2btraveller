49d9011b02933c53846d9f99e885ecfd75bdb231
Shivani Pandey <shivani@ipayments.org.in>
2026-01-12 11:12:03 +0530

    bus view details done

--- resources/views/bus/index-bus.blade.php
+++ resources/views/bus/index-bus.blade.php
@@ -299,7 +299,7 @@
 
 
 @push('script')
-    <script src="{{ asset('') }}js/bustrips.js"></script>
+    <script src="{{ asset('') }}js/bustrip.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
