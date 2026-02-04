9b313184b9d633ce8b3dded5b027918480959207
Shivani Pandey <shivani@ipayments.org.in>
2026-02-04 17:52:22 +0530

    Int ssr done

--- public/js/flighttripbook.js
+++ public/js/flighttripbook.js
@@ -2269,7 +2269,7 @@ function getSSRDetailsInternationalRT(resultIndex, traceId) {
             /* ======================
                SEATS (MOST IMPORTANT)
             ====================== */
-            if (ssr?.SeatDynamic && ssr?.SeatDynamic.length > 0) {
+            if (Array.isArray(ssr?.SeatDynamic) &&  ssr.SeatDynamic.length > 0) {
                 if (ssr.SeatDynamic?.[0]) {
                     renderSeatLayout([ssr.SeatDynamic[0]], totalPassengers, 'departure');
                 }
