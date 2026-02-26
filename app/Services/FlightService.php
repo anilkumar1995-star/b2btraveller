740c76cf92d5a88125c0fc43f65c9b147f99d900
Shivani Pandey <shivani@ipayments.org.in>
2026-02-26 16:52:02 +0530

    Int ssr done

--- app/Services/FlightService.php
+++ app/Services/FlightService.php
@@ -312,7 +312,7 @@ class FlightService
                 "Passengers" => $data['passengers'],
                 "isLlc" => $data['islcc'],
                 "clientRefId" => 'FLIGHT263176',
-                "amount" => 64000,
+                "amount" => '64000',
             ];
 
 
