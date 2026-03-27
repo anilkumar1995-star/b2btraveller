6847c052eae47715b9ceb5110102f416e3cb7653
Shivani Pandey <shivani@ipayments.org.in>
2026-03-27 11:14:07 +0530

    Int ssr done

--- resources/views/bus/review_booking.blade.php
+++ resources/views/bus/review_booking.blade.php
@@ -109,7 +109,7 @@
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf417-js/2.1.7/pdf417.min.js"></script> --}}
     <script src="https://unpkg.com/bwip-js/dist/bwip-js-min.js"></script>
 
-    <script src="{{ asset('') }}js/bustrippbooking.js"></script>
+    <script src="{{ asset('') }}js/busbbooking.js"></script>
     <script>
         $(document).ready(function() {
             const payload = JSON.parse(localStorage.getItem('payload'));
